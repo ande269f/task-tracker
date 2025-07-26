585c806b6032b742a78908fab46fa693dc200ba5
ande269f <andersknudsen98@gmail.com>
2025-07-26 19:26:13 +0200

    håndtering af submit af samme input flere gange

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,15 +4,29 @@ import { AppDispatch } from "../store";
 import { setTextInput } from "../store/slices/textInputSlice";
 import { useState } from "react";
 
+
+
 const TextInput = () => {
   const dispatch: AppDispatch = useDispatch();
-
   const [localInput, setLocalInput] = useState<string>("");
+
+  // håndtering af submit af samme værdi flere gange
+  const [PreviousInput, setPreviousInput] = useState<string>("");
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>, prevInput = PreviousInput) => {
+      e.preventDefault(); 
+    if (localInput != prevInput) {
+      dispatch(
+        setTextInput({
+          taskText: localInput, 
+          taskState: false }))
+        setPreviousInput(localInput);
+    }
+  }
 
 
   return (
     <> 
-    <form onSubmit={(e) => {e.preventDefault(); dispatch(setTextInput({ taskText: localInput, taskState: false }))}}>
+    <form onSubmit={handleSubmit}>
       <Group attached w="full" maxW="sm">
         <Input flex="1" placeholder="Indtast en todo" onChange={(e) => setLocalInput(e.target.value)} />
         <Button type="submit" bg="bg.subtle" variant="outline">
