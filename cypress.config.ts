abc29fcd6602b7fc2be2712cdaf0abd8102e3993
ande269f <andersknudsen98@gmail.com>
2025-10-27 13:06:49 +0100

    Opdater README [skip ci]

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    specPattern: "cypress/e2e/**/*.cy.ts",
+    supportFile: "cypress/support/e2e.ts",
     defaultCommandTimeout: 20000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    videosFolder: "cypress/videos",
+    screenshotsFolder: "cypress/screenshots",
+    // videoUploadOnPasses findes ikke i TS config – det styres nu af CI/upload
   },
 });
