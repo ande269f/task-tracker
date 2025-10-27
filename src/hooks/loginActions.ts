da0fa371bd3cc250d25fa5bcafc92a20638471a7
ande269f <andersknudsen98@gmail.com>
2025-10-27 16:29:33 +0100

    fjerner ubrugte deklarationer

--- src/hooks/loginActions.ts
+++ src/hooks/loginActions.ts
@@ -2,11 +2,9 @@ import { useDispatch } from "react-redux";
 import { AppDispatch } from "../store";
 import { login } from "../store/slices/loginSlice/thunks";
 import { setUsername } from "../store/slices/loginSlice/loginSlice";
-import { useState } from "react";
 
 export const useLoginActions = () => {
   const dispatch: AppDispatch = useDispatch();
-  const [loading, setLoading] = useState(false);
 
 
   const onSubmit = (data: { username: string; password: string }) => {
